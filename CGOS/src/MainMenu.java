4d4501e56fc7a6c7f47f7f439be6c6e47eba55c2
thechrizilla <chrisck.chan@hotmail.com>
2020-04-11 00:08:04 -0400

    updated MainMenu and OrderNewIngredient with Back Buttons

--- CGOS/src/MainMenu.java
+++ CGOS/src/MainMenu.java
@@ -1,100 +1,81 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class MainMenu extends JFrame {
 
-public class MainMenu {
+	private JPanel contentPane;
 
-		private JFrame frame;
-
-		/**
-		 * Launch the application.
-		 */
-		public static void main(String[] args) {
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						MainMenu window = new MainMenu();
-						window.frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		}
-
-		/**
-		 * Create the application.
-		 */
-		public MainMenu() {
-			initialize();
-		}
-
-		/**
-		 * Initialize the contents of the frame.
-		 */
-		private void initialize() {
-			frame = new JFrame();
-			frame.setBounds(100, 100, 450, 300);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.getContentPane().setLayout(null);
-			
-			JButton btnNewButton = new JButton("Ingredient Counter");
-			btnNewButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					IngredientCounter IC = new IngredientCounter();
-					IC.setVisible(true);
-					frame.dispose();
-					
-				}
-			});
-			btnNewButton.setBounds(158, 91, 177, 29);
-			frame.getContentPane().add(btnNewButton);
-			
-			JButton btnNewButton_1 = new JButton("Create Grocery");
-			btnNewButton_1.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					CreateGrocery CG = new CreateGrocery();
-					CG.setVisible(true);
-					frame.dispose();
-				}
-			});
-			btnNewButton_1.setBounds(6, 91, 152, 29);
-			frame.getContentPane().add(btnNewButton_1);
-			
-			JButton btnNewButton_2 = new JButton("Order New Ingredients");
-			btnNewButton_2.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					OrderNewIngredient ONI = new OrderNewIngredient();
-					ONI.setVisible(true);
-					frame.dispose();
-				}
-			});
-			btnNewButton_2.setBounds(57, 132, 196, 29);
-			frame.getContentPane().add(btnNewButton_2);
-			
-			JButton btnNewButton_3 = new JButton("View and Modify Budgets");
-			btnNewButton_3.setBounds(251, 132, 152, 29);
-			frame.getContentPane().add(btnNewButton_3);
-			
-			JButton btnNewButton_4 = new JButton("Auto Adjust Budgets");
-			btnNewButton_4.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
-			btnNewButton_4.setBounds(327, 91, 117, 29);
-			frame.getContentPane().add(btnNewButton_4);
-			
-			JButton btnNewButton_5 = new JButton("Chris is the Best");
-			btnNewButton_5.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
-			btnNewButton_5.setBounds(131, 184, 202, 29);
-			frame.getContentPane().add(btnNewButton_5);
-		}
-
+			}
+		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainMenu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton CreateGroceryButton = new JButton("Create Grocery");
+		CreateGroceryButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CreateGrocery CG = new CreateGrocery();
+				CG.setVisible(true);
+				dispose();
+			}
+		});
+		CreateGroceryButton.setBounds(162, 57, 117, 29);
+		contentPane.add(CreateGroceryButton);
+		
+		JButton OrderNewIngredientButton = new JButton("Order New Ingredient");
+		OrderNewIngredientButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OrderNewIngredient ONI = new OrderNewIngredient();
+				ONI.setVisible(true);
+				dispose();
+			}
+		});
+		OrderNewIngredientButton.setBounds(134, 21, 171, 29);
+		contentPane.add(OrderNewIngredientButton);
+		
+		JButton ViewModifyBudgets = new JButton("View/Modify Restaurant Budgets");
+		ViewModifyBudgets.setBounds(101, 95, 240, 29);
+		contentPane.add(ViewModifyBudgets);
+		
+		JButton AutoAdjustBudgets = new JButton("Auto Adjust Budgets");
+		AutoAdjustBudgets.setBounds(134, 132, 160, 29);
+		contentPane.add(AutoAdjustBudgets);
+		
+		JButton ViewModifyRestaurantOrdersButton = new JButton("View/Modify Restaurant Orders");
+		ViewModifyRestaurantOrdersButton.setBounds(101, 173, 229, 29);
+		contentPane.add(ViewModifyRestaurantOrdersButton);
+		
+		JButton QuitButton = new JButton("Quit");
+		QuitButton.setBounds(162, 214, 117, 29);
+		contentPane.add(QuitButton);
+	}
+
+}
--- CGOS/src/OrderNewIngredient.java
+++ CGOS/src/OrderNewIngredient.java
@@ -4,37 +4,79 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class OrderNewIngredient extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField IngredientTypeTextField;
+	private JTextField WeightTextfield;
+	MainMenu MM = new MainMenu();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					OrderNewIngredient frame = new OrderNewIngredient();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public OrderNewIngredient() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Ship Name:");
+		lblNewLabel.setBounds(129, 66, 71, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Restaurant Name:");
+		lblNewLabel_1.setBounds(89, 94, 111, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Ingredient Type:");
+		lblNewLabel_2.setBounds(98, 122, 102, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Weight:");
+		lblNewLabel_3.setBounds(153, 150, 47, 16);
+		contentPane.add(lblNewLabel_3);
+		
+		JComboBox ShipNameComboBox = new JComboBox();
+		ShipNameComboBox.setBounds(212, 62, 130, 27);
+		contentPane.add(ShipNameComboBox);
+		
+		JComboBox RestaurantComboBox = new JComboBox();
+		RestaurantComboBox.setBounds(212, 90, 130, 27);
+		contentPane.add(RestaurantComboBox);
+		
+		IngredientTypeTextField = new JTextField();
+		IngredientTypeTextField.setBounds(212, 117, 130, 26);
+		contentPane.add(IngredientTypeTextField);
+		IngredientTypeTextField.setColumns(10);
+		
+		WeightTextfield = new JTextField();
+		WeightTextfield.setBounds(212, 145, 130, 26);
+		contentPane.add(WeightTextfield);
+		WeightTextfield.setColumns(10);
+		
+		JButton OrderButton = new JButton("Order");
+		OrderButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		OrderButton.setBounds(169, 219, 117, 29);
+		contentPane.add(OrderButton);
+		
+		JButton BackButton = new JButton("Back");
+		BackButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainMenu MM = new MainMenu();
+				MM.setVisible(true);
 
+			}
+		});
+		BackButton.setBounds(6, 243, 117, 29);
+		contentPane.add(BackButton);
+	}
 }
